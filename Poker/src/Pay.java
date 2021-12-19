71ea1274a3e504a61738263567cec39e96030810
Sky Cen <censky2@gmail.com>
2021-12-19 01:07:18 -0500

    Added Pay.java

---
+++ Poker/src/Pay.java
@@ -0,0 +1,49 @@
+
+import java.util.Scanner;
+import java.math.BigDecimal;
+
+public class Pay {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter number of players: ");
+		int numPlayers = scan.nextInt();
+		System.out.print("Enter buy in: ");
+		BigDecimal buyIn = new BigDecimal(scan.nextDouble());
+		BigDecimal[] database = new BigDecimal[numPlayers];
+		String[] database2 = new String[numPlayers];
+		BigDecimal endTotal = new BigDecimal(0);
+		int loserIndex = 0;
+		BigDecimal loserAmount = new BigDecimal(numPlayers).multiply(buyIn);
+		BigDecimal totalAmount = new BigDecimal(numPlayers).multiply(buyIn);
+
+		for (int i = 1; i <= numPlayers; i++) {
+			System.out.print("Enter player " + i + " name: ");
+			database2[i - 1] = scan.next();
+			System.out.print("Enter " + database2[i - 1] + " end balance: ");
+			database[i - 1] = new BigDecimal(scan.nextDouble());
+			endTotal = endTotal.add(database[i - 1]);
+			if (database[i - 1].compareTo(loserAmount) < 1) {
+				loserAmount = database[i - 1];
+				loserIndex = i - 1;
+			}
+		}
+
+		if (endTotal.compareTo(totalAmount) != 0) {
+			System.out.println("Error: end balance incorrect!");
+		} else {
+			for (int i = 0; i < numPlayers; i++) {
+				if (i != loserIndex && database[i].compareTo(buyIn) < 0) {
+					System.out.println(
+							database2[i] + " pays " + database2[loserIndex] + " $" + buyIn.subtract(database[i]));
+				} else if (i == loserIndex) {
+					System.out.println(database2[i] + " pays everyone");
+				} else {
+					System.out.println(database2[i] + " receives $" + database[i].subtract(buyIn) + " from "
+							+ database2[loserIndex]);
+				}
+			}
+		}
+
+		scan.close();
+	}
+}
--- Poker/src/PokerHand.java
+++ Poker/src/PokerHand.java
@@ -1,65 +1,62 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+
 public class PokerHand {
-	
-	//maybe have this return the card value with the pair instead of boolean
-	//since value of the pair is relevant
-	public static HashMap<Integer,Integer> hasPair(ArrayList<Card> cards) {
-		HashMap<Integer, Integer> result = new HashMap<Integer,Integer>();
-		for (Card c: cards) {
-			for (Card d: cards) {
+
+	// maybe have this return the card value with the pair instead of boolean
+	// since value of the pair is relevant
+	public static HashMap<Integer, Integer> hasPair(ArrayList<Card> cards) {
+		HashMap<Integer, Integer> result = new HashMap<Integer, Integer>();
+		for (Card c : cards) {
+			for (Card d : cards) {
 				if (c.getValue() == d.getValue() && d != c) {
 					result.put(c.getValue(), c.getSuit());
 					return result;
 				}
 			}
 		}
-		
+
 		return null;
 	}
-	
-	
+
 	public static int highCard(ArrayList<Card> cards) {
 		int max = 0;
-		for(Card c: cards) {
-			if(c.getValue() > max) {
+		for (Card c : cards) {
+			if (c.getValue() > max) {
 				max = c.getValue();
 			}
 		}
 		return max;
 	}
-	
-	
-	
+
 	public static boolean threeOfAKind(ArrayList<Card> cards) {
-		for(Card c: cards) {
-			if(countOccur(cards, c.getValue()) == 3) {
+		for (Card c : cards) {
+			if (countOccur(cards, c.getValue()) == 3) {
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	public static boolean fourOfAKind(ArrayList<Card> cards) {
-		for(Card c: cards) {
-			if(countOccur(cards, c.getValue()) == 4) {
+		for (Card c : cards) {
+			if (countOccur(cards, c.getValue()) == 4) {
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
-	
+
 //	public static boolean twoPair(ArrayList<Card> cards) {
 //		
 //	}
-	//counts number of times value is in card ArrayList
+	// counts number of times value is in card ArrayList
 	private static int countOccur(ArrayList<Card> cards, int value) {
 		int count = 0;
-		for(Card c: cards) {
-			if(c.getValue() == value) {
+		for (Card c : cards) {
+			if (c.getValue() == value) {
 				count++;
 			}
 		}
