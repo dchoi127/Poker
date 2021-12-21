c3d8ccc51c8b7d9bb7fee0840e8b2a523ed8536c
Sky Cen <censky2@gmail.com>
2021-12-20 23:50:17 -0500

    Added generateHands method

--- Poker/src/model/PokerHand.java
+++ Poker/src/model/PokerHand.java
@@ -59,7 +59,7 @@ public class PokerHand {
 	} };
 
 	private ArrayList<Card> cards;
-
+	
 	public PokerHand() {
 		cards = new ArrayList<>();
 	}
@@ -75,6 +75,11 @@ public class PokerHand {
 
 		cards.add(card);
 		return true;
+	}
+	
+	// Returns copy of cards 
+	public ArrayList<Card> getCards() {
+		return new ArrayList<Card>(cards);
 	}
 
 	// included an exclude param
@@ -413,5 +418,50 @@ public class PokerHand {
 			}
 		}
 		return count;
+	}
+	
+	/* n is number of cards in deck 
+	 * r is number of cards generated per hand
+	 * 
+	 * I think it is easier to do n = 52 (so values from 1 - 52) 
+	 * and then split the suits up (1 - 13 is hearts, 14 - 26 is spades etc).
+	 * Then we remove all hands that include the cards in the user's hand in another method?
+	 * 
+	 * public for testing reasons rn
+	 */
+	public static ArrayList<Card[]> generateHands(int n, int r) {
+		ArrayList<Card[]> hands=  new ArrayList<>();
+		Card[] hand = new Card[r];
+		
+		for (int i = 0; i < r; i++) {
+			hand[i] = new Card(i, 1);
+		}
+		
+		int i = r - 1;
+		while (hand[0].getValue() < n - r + 1) {
+			while (i > 0 && hand[i].getValue() == n - r + 1) {
+				i--;
+			}
+			
+			Card[] handCopy = new Card[r];
+			for (int j = 0; j < hand.length; j++) {
+				int value = hand[j].getValue();
+				int suit = value / 13 + 1;
+				
+				value = value % 13 + 1;
+				
+				handCopy[j] = new Card(value, suit);
+			}
+			hands.add(handCopy);
+			
+			hand[i].setValue(hand[i].getValue() + 1);
+			
+			while (i < r - 1) {
+				hand[i + 1].setValue(hand[i].getValue() + 1);
+				i++;
+			}
+		}
+		
+		return hands;
 	}
 }
